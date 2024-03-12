99a5f629fd9efb5626a3f1210f2264a5641a1ac3
ahmedGDI <guedria.guedri@gmail.com>
2024-03-12 13:49:52 +0100

    connected secuss in back-end and prepare all the struc

---
+++ back-end/routes/users.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+
+
+const {} = require('../controllers/users')
+
+
+router.route('/users').get()
+
+
+router.post('/users/signup',)
+router.post('/users/signin',)
+
+
+
+
+module.exports = router
